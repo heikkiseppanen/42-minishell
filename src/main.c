2442a2a843768aecc2fc36a448f9b3fc4cc43ffc
lsileoni <lsileoni@proton.me>
2023-04-05 18:31:46 +0300

    Using termios instead of manual control of the terminal to achieve identical effect to bash

--- src/main.c
+++ src/main.c
@@ -6,26 +6,33 @@
 /*   By: hseppane <marvin@42.ft>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 08:38:53 by hseppane          #+#    #+#             */
-/*   Updated: 2023/03/20 09:35:19 by lsileoni         ###   ########.fr       */
+/*   Updated: 2023/04/05 18:30:41 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sig.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <termios.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
 int	main(void)
 {
+	struct termios t;
 	char	*input;
 
+	tcgetattr(0, &t);
 	rl_bind_key('\t', rl_complete);
 	using_history();
 	init_sighandler();
 	while (1)
 	{
+		t.c_lflag &= ~ECHOCTL;
+		tcsetattr(0, TCSANOW, &t);
 		input = readline("> ");
+		t.c_lflag |= ECHOCTL;
+		tcsetattr(0, TCSANOW, &t);
 		if (!input)
 			break ;
 		add_history(input);
--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: lsileoni <lsileoni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 10:29:23 by lsileoni          #+#    #+#             */
-/*   Updated: 2023/04/05 17:29:22 by lsileoni         ###   ########.fr       */
+/*   Updated: 2023/04/05 18:26:28 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,18 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 
-static void	delete_esc_char(void)
-{
-	write(1, "\033[1C\033[K", 8);
-}
-
 static void	return_prompt(void)
 {
 	write(1, "\n", 1);
-	rl_on_new_line();
 	rl_replace_line("", 0);
+	rl_on_new_line();
 	rl_redisplay();
 }
 
 static void	sig_handler(int signum)
 {
 	if (signum == SIGINT)
-	{
-		delete_esc_char();
 		return_prompt();
-	}
 }
 
 void	init_sighandler(void)
