013e385796be4106adf1035ff15710dc05fd161b
Heikki Seppänen <6179953+heikkiseppanen@users.noreply.github.com>
2023-05-11 16:05:06 +0300

    Cleaned up redirection parsing

--- src/parser/parse_parameters.c
+++ src/parser/parse_parameters.c
@@ -6,11 +6,88 @@
 /*   By: hseppane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 10:00:33 by hseppane          #+#    #+#             */
-/*   Updated: 2023/05/04 20:24:24 by hseppane         ###   ########.fr       */
+/*   Updated: 2023/05/11 15:26:17 by hseppane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ast.h"
+
+
+static e_err	parse_redir_target(t_token **iterator, int *out)
+{
+	if (token_is(*iterator, TOK_GREAT | TOK_DGREAT))
+	{
+		*out = STDOUT_FILENO;
+	}
+	else if (token_is(*iterator, TOK_LESS | TOK_DLESS))
+	{
+		*out = STDIN_FILENO;
+	}
+	else if (token_is(*iterator, TOK_IO_DIGIT))
+	{
+		*out = (int)(*(*iterator)->begin - '0');
+		*iterator += 1;
+	}
+	else
+	{
+		return (unexpect(*iterator));
+	}
+	return (MS_SUCCESS);
+}
+
+static e_err	parse_redir_op(t_token **iterator, e_redir_op *out)
+{
+	if (token_is(*iterator, TOK_GREAT))
+	{
+		*out = REDIR_OUT_TRUNC;
+	}
+	else if (token_is(*iterator, TOK_DGREAT))
+	{
+		*out = REDIR_OUT_APPEND;
+	}
+	else if (token_is(*iterator, TOK_LESS))
+	{
+		*out = REDIR_IN_FILE;
+	}
+	else if (token_is(*iterator, TOK_DLESS))
+	{
+		*out = REDIR_IN_HEREDOC;
+	}
+	else
+	{
+		return (unexpect(*iterator));
+	}
+	*iterator += 1;
+	return (MS_SUCCESS);
+}
+
+static e_err	parse_redir_arg(t_token **iterator, const char **out)
+{
+	if (!token_is(*iterator, TOK_WORD))
+	{
+		return (unexpect(*iterator));
+	}
+	*out = token_to_str(*iterator);
+	if (!*out)
+	{
+		return (MS_FAIL);
+	}
+	return (MS_SUCCESS);
+}
+
+static e_err	parse_redirection(t_token **iterator, t_buf *redir_out)
+{
+	t_redir	redirection;
+
+	if (parse_redir_target(iterator, &redirection.file_descriptor) == MS_FAIL
+		|| parse_redir_op(iterator, &redirection.operation) == MS_FAIL
+		|| parse_redir_arg(iterator, &redirection.argument) == MS_FAIL
+		|| !buf_pushback(redir_out, &redirection, 1))
+	{
+		return (MS_FAIL);
+	}
+	return (MS_SUCCESS);
+}
 
 static e_err	parse_argument(t_token **iterator, t_buf *argv_out)
 {
@@ -19,47 +96,9 @@ static e_err	parse_argument(t_token **iterator, t_buf *argv_out)
 	argument = token_to_str(*iterator);
 	*iterator += 1;
 	if (!buf_pushback(argv_out, &argument, 1))
-		return (MS_FAIL);
-	return (MS_SUCCESS);
-}
-
-static e_err	parse_redirection(t_token **iterator, t_buf *redir_out)
-{
-	t_redir	redirection;
-
-	if (token_is(*iterator, TOK_GREAT | TOK_DGREAT))
-		redirection.file_descriptor = STDOUT_FILENO;
-	else if (token_is(*iterator, TOK_LESS | TOK_DLESS))
-		redirection.file_descriptor = STDIN_FILENO;
-	else if (token_is(*iterator, TOK_IO_DIGIT))
 	{
-		redirection.file_descriptor = (int)(*(*iterator)->begin - '0');
-		*iterator += 1;
-	}
-	else
-		return (unexpect(*iterator));
-
-	if (token_is(*iterator, TOK_GREAT))
-		redirection.operation = REDIR_OUT_TRUNC;
-	else if (token_is(*iterator, TOK_DGREAT))
-		redirection.operation = REDIR_OUT_APPEND;
-	else if (token_is(*iterator, TOK_LESS))
-		redirection.operation = REDIR_IN_FILE;
-	else if (token_is(*iterator, TOK_DLESS))
-		redirection.operation = REDIR_IN_HEREDOC;
-	else
-		return (unexpect(*iterator));
-
-	*iterator += 1;
-
-	if (!token_is(*iterator, TOK_WORD))
-		return (unexpect(*iterator));
-	redirection.argument = token_to_str(*iterator);
-
-	*iterator += 1;
-
-	if (!buf_pushback(redir_out, &redirection, 1))
 		return (MS_FAIL);
+	}
 	return (MS_SUCCESS);
 }
 
