072091db757c0406a4c4576a04bc9ce09bfedf63
lsileoni <lsileoni@proton.me>
2023-05-12 20:19:11 +0300

    continued breakdown of string expansion, functions as expected

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: lsileoni <lsileoni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 22:45:25 by lsileoni          #+#    #+#             */
-/*   Updated: 2023/05/12 18:09:31 by lsileoni         ###   ########.fr       */
+/*   Updated: 2023/05/12 20:17:35 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,105 +17,146 @@
 
 extern t_shell_state g_state;
 
-typedef struct s_exp_state
+typedef struct s_sym_state
 {
-	char	openchar;
 	t_buf	*buf;
 	int		open;
 	int		i;
-}	t_exp_state;
+	char	openchar;
+}	t_sym_state;
 
-int	handle_exp(t_exp_state *s, const char *string)
+typedef struct s_exp_state
 {
 	char	*env;
 	char	*envres;
+	int		i;
+}	t_exp_state;
+
+void	check_underscore(t_sym_state *s_s, t_exp_state *e_s, const char *string)
+{
+	if (string[s_s->i] == '_')
+	{
+		e_s->env[e_s->i] = '_';
+		e_s->i++;
+		s_s->i++;
+	}
+}
+
+int	check_questionmark(t_sym_state *s, char *envres, char *env)
+{
+	envres = ft_itoa(g_state.pipeline_err);
+	if (!envres)
+	{
+		buf_del(s->buf);
+		free(env);
+		return (0);
+	}
+	if (!buf_pushback(s->buf, envres, ft_strlen(envres)))
+	{
+		buf_del(s->buf);
+		free(env);
+		free(envres);
+		return (0);
+	}
+	free(envres);
+	s->i++;
+	return (1);
+}
+
+static char	*alloc_env(t_sym_state *s, const char *string)
+{
+	char	*res;
 	int		i;
 
 	i = s->i;
 	while (string[i] && ft_isalnum(string[i]))
 		i++;
-	env = malloc((i - s->i) + 2);
-	if (!env)
+	res = ft_calloc(i + 2, 1);
+	if (!res)
 	{
 		buf_del(s->buf);
+		return (NULL);
+	}
+	return (res);
+}
+
+static int	check_null(t_sym_state *s, char *env)
+{
+	if (!buf_pushback(s->buf, "$", 1))
+	{
+		buf_del(s->buf);
+		free(env);
 		return (0);
 	}
-	i = 0;
-	if (string[s->i] == '?')
+	return (1);
+}
+
+static int	check_other(t_sym_state *s, char *env, const char *string)
+{
+	if (!buf_pushback(s->buf, (void *)((&string[s->i])-1), 2))
 	{
-		envres = ft_itoa(g_state.pipeline_err);
-		if (!envres)
-		{
-			buf_del(s->buf);
-			free(env);
-			return (0);
-		}
-		if (!buf_pushback(s->buf, envres, ft_strlen(envres)))
-		{
-			buf_del(s->buf);
-			free(env);
-			free(envres);
-			return (0);
-		}
-		free(envres);
-		s->i++;
-		return (1);
+		buf_del(s->buf);
+		free(env);
+		return (0);
 	}
-	else if (string[s->i] == '_')
-	{
-		env[i] = '_';
-		i++;
-		s->i++;
-	}
+	s->i++;
+	return (1);
+}
+
+static int	check_first_tok(t_sym_state *s_s, t_exp_state *e_s, const char *string)
+{
+	if (string[s_s->i] == '?')
+		return (check_questionmark(s_s, e_s->envres, e_s->env));
 	else
 	{
-		if (!string[s->i])
-		{
-			if (!buf_pushback(s->buf, "$", 1))
-			{
-				buf_del(s->buf);
-				free(env);
-				return (0);
-			}
-			return (1);
-		}
-		if (!ft_isalpha(string[s->i]))
-		{
-			if (!buf_pushback(s->buf, (void *)((&string[s->i])-1), 2))
-			{
-				buf_del(s->buf);
-				free(env);
-				return (0);
-			}
-			s->i++;
-			return (1);
-		}
+		if (!string[s_s->i])
+			return (check_null(s_s, e_s->env));
+		if (!ft_isalpha(string[s_s->i]))
+			return (check_other(s_s, e_s->env, string));
 	}
-	i = 0;
-	while (string[s->i] && ft_isalnum(string[s->i]))
+	return (0);
+}
+
+static void	grab_env_str(t_sym_state *s_s, t_exp_state *e_s, const char *string)
+{
+	e_s->i = 0;
+	while (string[s_s->i] && ft_isalnum(string[s_s->i]))
 	{
-		env[i] = string[s->i];
-		s->i++;
-		i++;
+		e_s->env[e_s->i] = string[s_s->i];
+		s_s->i++;
+		e_s->i++;
 	}
-	env[i] = '\0';
-	envres = ft_htable_get(g_state.envp, env);
-	free(env);
-	if (!envres)
+	e_s->env[e_s->i] = '\0';
+}
+
+int	handle_exp(t_sym_state *s_s, const char *string)
+{
+	t_exp_state e_s;
+
+	e_s.env = alloc_env(s_s, string);
+	if (!e_s.env)
+		return (0);
+	if (string[s_s->i] == '?' || !string[s_s->i] || !ft_isalpha(string[s_s->i]))
+		return (check_first_tok(s_s, &e_s, string));
+	check_underscore(s_s, &e_s, string);
+	grab_env_str(s_s, &e_s, string);
+	e_s.envres = ft_htable_get(g_state.envp, e_s.env);
+	free(e_s.env);
+	if (!e_s.envres)
 		return (1);
-	if (!buf_pushback(s->buf, envres, ft_strlen(envres)))
+	if (!buf_pushback(s_s->buf, e_s.envres, ft_strlen(e_s.envres)))
 	{
-		buf_del(s->buf);
+		buf_del(s_s->buf);
 		return (0);
 	}
-	if (!string[s->i])
+	if (!string[s_s->i])
 		return (2);
 	return (1);
 }
 
 char	*str_expand(const char *string)
 {
-	t_exp_state	s;
+	t_sym_state	s;
 	int			rval;
 	char		*expanded;
 
