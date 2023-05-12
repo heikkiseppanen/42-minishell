56ef183faaca6a246b170216664f712d35f861d8
lsileoni <lsileoni@proton.me>
2023-05-12 18:27:50 +0300

    reformatted expansion from string pointer manipulation to state indexing

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: lsileoni <lsileoni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 22:45:25 by lsileoni          #+#    #+#             */
-/*   Updated: 2023/05/11 20:43:51 by lsileoni         ###   ########.fr       */
+/*   Updated: 2023/05/12 18:09:31 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,26 @@ typedef struct s_exp_state
 	char	openchar;
 	t_buf	*buf;
 	int		open;
+	int		i;
 }	t_exp_state;
 
-int	handle_exp(t_exp_state *s, char **string)
+int	handle_exp(t_exp_state *s, const char *string)
 {
 	char	*env;
 	char	*envres;
 	int		i;
 
-	i = 0;
-	while ((*string)[i] && ft_isalnum((*string)[i]))
+	i = s->i;
+	while (string[i] && ft_isalnum(string[i]))
 		i++;
-	env = malloc(i + 1);
+	env = malloc((i - s->i) + 2);
 	if (!env)
 	{
 		buf_del(s->buf);
 		return (0);
 	}
 	i = 0;
-	if (**string == '?')
+	if (string[s->i] == '?')
 	{
 		envres = ft_itoa(g_state.pipeline_err);
 		if (!envres)
@@ -57,20 +58,20 @@ int	handle_exp(t_exp_state *s, char **string)
 			return (0);
 		}
 		free(envres);
-		(*string)++;
+		s->i++;
 		return (1);
 	}
-	else if (**string == '_')
+	else if (string[s->i] == '_')
 	{
 		env[i] = '_';
 		i++;
-		(*string)++;
+		s->i++;
 	}
 	else
 	{
-		if (!ft_isalpha(**string))
+		if (!string[s->i])
 		{
-			if (!buf_pushback(s->buf, (void *)(string[-1]), 1))
+			if (!buf_pushback(s->buf, "$", 1))
 			{
 				buf_del(s->buf);
 				free(env);
@@ -78,11 +79,23 @@ int	handle_exp(t_exp_state *s, char **string)
 			}
 			return (1);
 		}
+		if (!ft_isalpha(string[s->i]))
+		{
+			if (!buf_pushback(s->buf, (void *)((&string[s->i])-1), 2))
+			{
+				buf_del(s->buf);
+				free(env);
+				return (0);
+			}
+			s->i++;
+			return (1);
+		}
 	}
-	while (**string && ft_isalnum(**string))
+	i = 0;
+	while (string[s->i] && ft_isalnum(string[s->i]))
 	{
-		env[i] = **string;
-		(*string)++;
+		env[i] = string[s->i];
+		s->i++;
 		i++;
 	}
 	env[i] = '\0';
@@ -95,42 +108,41 @@ int	handle_exp(t_exp_state *s, char **string)
 		buf_del(s->buf);
 		return (0);
 	}
-	if (!(*string))
+	if (!string[s->i])
 		return (2);
 	return (1);
 }
 
-
-
 char	*str_expand(const char *string)
 {
-	t_exp_state	state;
+	t_exp_state	s;
 	int			rval;
 	char		*expanded;
 
-	state.open = -1;
-	state.openchar = 0;
-	state.buf = malloc(sizeof(t_buf));
-	if (!state.buf || !(buf_init(state.buf, 1024, 1)))
+	s.i = 0;
+	s.open = -1;
+	s.openchar = 0;
+	s.buf = malloc(sizeof(t_buf));
+	if (!s.buf || !(buf_init(s.buf, 1024, 1)))
 		return (NULL);
-	while(*string)
+	while(string[s.i])
 	{
-		if ((*string == '"' || *string == '\'') && state.open < 0)
+		if ((string[s.i] == '"' || string[s.i] == '\'') && s.open < 0)
 		{
-			state.openchar = '"';
-			if (*string == '\'')
-				state.openchar = '\'';
-			state.open = 1;
+			s.openchar = '"';
+			if (string[s.i] == '\'')
+				s.openchar = '\'';
+			s.open = 1;
 		}
-		else if ((*string == state.openchar) && state.open > 0)
+		else if ((string[s.i] == s.openchar) && s.open > 0)
 		{
-			state.openchar = 0;
-			state.open = -1;
+			s.openchar = 0;
+			s.open = -1;
 		}
-		else if ((*string == '$') && (state.openchar == '"' || state.open < 0))
+		else if ((string[s.i] == '$') && (s.openchar == '"' || s.open < 0))
 		{
-			string++;
-			rval = handle_exp(&state, (char **)&string);
+			s.i++;
+			rval = handle_exp(&s, string);
 			if (!rval)
 				return (NULL);
 			if (rval == 1)
@@ -139,15 +151,15 @@ char	*str_expand(const char *string)
 		}
 		else
 		{
-			if (!buf_pushback(state.buf, (void *)string, 1))
+			if (!buf_pushback(s.buf, (void *)(&string[s.i]), 1))
 				return (NULL);
 		}
-		string++;
+		s.i++;
 	}
-	if (!buf_pushback(state.buf, "", 1))
+	if (!buf_pushback(s.buf, "", 1))
 		return (NULL);
-	expanded = ft_strdup((char *)state.buf->data);
-	buf_del(state.buf);
+	expanded = ft_strdup((char *)s.buf->data);
+	buf_del(s.buf);
 	return (expanded);
 }
 
