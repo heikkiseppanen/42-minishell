f0126fb4b571cbe8124c036e06b4f9b5ccf8edcf
lsileoni <lsileoni@proton.me>
2023-05-13 05:08:02 +0300

    added error handling for cd and also made it function

--- src/builtins/b_cd.c
+++ src/builtins/b_cd.c
@@ -6,10 +6,11 @@
 /*   By: lsileoni <lsileoni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 04:07:10 by lsileoni          #+#    #+#             */
-/*   Updated: 2023/05/13 04:11:02 by lsileoni         ###   ########.fr       */
+/*   Updated: 2023/05/13 05:06:10 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <sys/fcntl.h>
 #include <sys/stat.h>
 #include <stdlib.h>
@@ -19,6 +20,32 @@
 
 extern t_shell_state	g_state; 
 
+int	go_to_dir(struct stat info, char **argv, char *buf)
+{
+	char	*old_pwd = ft_strdup(ft_htable_get(g_state.envp, "PWD"));
+
+	old_pwd = ft_strdup(ft_htable_get(g_state.envp, "PWD"));
+	if (S_ISDIR(info.st_mode) && old_pwd)
+	{
+		if (chdir(argv[1]) != 0)
+		{
+			perror("cd");
+			free(buf);
+			return (1);
+		}
+		getcwd(buf, 1024);
+		ft_htable_insert(g_state.envp, "PWD", buf);
+		ft_htable_insert(g_state.envp, "OLDPWD", old_pwd);
+	}
+	else
+	{
+		perror("cd");
+		free(buf);
+		return (1);
+	}
+	return (0);
+}
+
 int	change_directory(char	**argv)
 {
 	struct stat	info;
@@ -26,22 +53,16 @@ int	change_directory(char	**argv)
 	char		*buf;
 
 	fd = open(argv[1], O_RDONLY); 
+	if (fd == -1)
+	{
+		perror("cd");
+		return (1);
+	}
 	fstat(fd, &info);
-	buf = malloc(1024);
 	close(fd);
-	if (!buf)
+	buf = malloc(1024);
+	if (!buf || go_to_dir(info, argv, buf))
 		return (1);
-	if (S_ISDIR(info.st_mode))
-	{
-		chdir(argv[1]);
-		getcwd(buf, 1024);
-		ft_htable_insert(g_state.envp, "PWD", buf);
-	}
-	else
-	{
-		free(buf);
-		return (1);
-	}
 	return (0);
 }
 
