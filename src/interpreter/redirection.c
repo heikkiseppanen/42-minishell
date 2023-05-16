4b9b489185be227aee63d14f9c40583afc05040b
lsileoni <lsileoni@proton.me>
2023-05-16 06:58:49 +0300

    redirection expansion

--- src/interpreter/redirection.c
+++ src/interpreter/redirection.c
@@ -6,12 +6,13 @@
 /*   By: hseppane <marvin@42.ft>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 09:54:05 by hseppane          #+#    #+#             */
-/*   Updated: 2023/05/10 18:01:27 by hseppane         ###   ########.fr       */
+/*   Updated: 2023/05/16 06:54:02 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "interpreter.h"
 #include "redir.h"
+#include "expand.h"
 
 #include <stdio.h>
 
@@ -19,6 +20,7 @@ e_err	perform_redirections(t_ast_node *redirections)
 {
 	const t_redir 	*it = redirections->data.redir.array;
 	const t_redir 	*end = it + redirections->data.redir.size;
+	t_redir 		*tmp;
 	e_err			status;
 
 	if (redirections->type != AST_REDIR)
@@ -28,7 +30,13 @@ e_err	perform_redirections(t_ast_node *redirections)
 	status = MS_SUCCESS;
 	while (it != end && status == MS_SUCCESS)
 	{
-		status = redir_execute(it);
+		tmp = (t_redir *)it;
+		tmp->argument = arg_expand(tmp->argument);
+		if (!tmp->argument)
+		{
+			return (MS_FAIL);
+		}
+		status = redir_execute(tmp);
 		++it;
 	}
 	return (status);
